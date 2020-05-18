f2a9c46e880d96fcad360200d1573bc9daf35ba9
Huan LI (李卓桓) <zixia@zixia.net>
2020-05-19 01:47:29 +0800

    code clean

--- examples/counter/selectors.ts
+++ examples/counter/selectors.ts
@@ -1,11 +1,3 @@
-/**
- * CQRS - Command Query Responsibility Segregation
- *  https://docs.microsoft.com/en-us/azure/architecture/patterns/cqrs
- *
- *  `selectors` for Query
- *
- *  Define your Read Models and projection functions / query resolvers
- */
 const getCounter       = (state: { total: number }) => () => state.total
 const getMeaningOfLife = (_state: any) => (_: any) => 42
 
